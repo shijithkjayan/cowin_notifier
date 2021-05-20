1f45c6c5375a3781f0302210acf304b196152458
Shijith <shijith.karumathil@blockfi.com>
2021-05-20 15:25:35 +0530

    initial commit

---
+++ lib/cowin_notifier.ex
@@ -0,0 +1,18 @@
+defmodule CowinNotifier do
+  @moduledoc """
+  Documentation for `CowinNotifier`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CowinNotifier.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cowin_notifier_test.exs
@@ -0,0 +1,8 @@
+defmodule CowinNotifierTest do
+  use ExUnit.Case
+  doctest CowinNotifier
+
+  test "greets the world" do
+    assert CowinNotifier.hello() == :world
+  end
+end
